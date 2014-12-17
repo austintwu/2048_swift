5ab9e1e0488134c4323c1b0bb06593c5d0fbf7af
Austin Wu <austin.wu@duke.edu>
2014-12-17 09:03:34 -0600

    Added background

--- Twenty48/GameScene.swift
+++ Twenty48/GameScene.swift
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014 Austin Wu. All rights reserved.
 //
 
+import UIKit
 import SpriteKit
 
 class GameScene: SKScene {
@@ -13,8 +14,38 @@ class GameScene: SKScene {
     let gameLayer = SKNode()
     let blockLayer = SKNode()
     
+    override init(size: CGSize){
+        super.init(size: size)
+
+        anchorPoint = CGPoint(x: 0.0, y: 1.0)
+        
+        let background = SKSpriteNode(texture: SKTexture(imageNamed: "Board Background"), size: CGSize(width: 375, height: 375))
+        background.position = CGPoint(x: 0, y: -146)
+        background.anchorPoint = CGPoint(x: 0, y: 1.0)
+        addChild(background)
+
+        /**
+
+        for var y = 0; y < 4; y++ {
+            for var x = 0; x < 4; x++ {
+                SKSpriteNode square =
+                CGPoint squareCoordinate = CGPointMake(x, y);
+                squareNode.position = [_squareSystemCoord tileCenter:squareCoordinate];
+                squareNode.texture = _textureNormal;
+                [self addChild:squareNode];
+            }
+        }
+        */
+        
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     /**
-    Background layer: Flat background, get color from game online
     Next layer: 4x4 grid of squares (gamelayer)
     Next layer: each individual block (blockLayer)
     
